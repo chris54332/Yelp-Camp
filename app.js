6fdc6b889d69d80e0842e439971a73a8cd1b35e9
Chris <chris54332@gmail.com>
2020-05-19 18:06:55 +0100

    .

--- app.js
+++ app.js
@@ -17,10 +17,8 @@ let methodOverride = require("method-override"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-const URI = "mongodb+srv://admin:pass@cluster0-v7x25.mongodb.net/test?retryWrites=true&w=majority";
 
-mongoose.connect(URI, { useNewUrlParser: true });
-
+mongoose.connect('mongodb://admin:pass@cluster0-shard-00-00-v7x25.mongodb.net:27017,cluster0-shard-00-01-v7x25.mongodb.net:27017,cluster0-shard-00-02-v7x25.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 app = express();
 
 app.set(express.static("public"));
